1140ad154e26ede4b57188cbfd2dd059c495b726
indir <indir.karabegovic@hotmail.com>
2022-05-18 10:33:57 +0200

    basic menu implemented

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,63 @@
 
 fun main() {
-    println("Enter side a: ")
-    val a = try {readLine()?.toDouble() } catch (e: NumberFormatException) { 0.0 }
-    println("Enter side b: ")
-    val b = try {readLine()?.toDouble() } catch (e: NumberFormatException) { 0.0 }
-    println("Enter side c: ")
-    val c = try {readLine()?.toDouble() } catch (e: NumberFormatException) { 0.0 }
-    val triangle1 = Triangle(a!!,b!!,c!!)
-    triangle1.getData()
-    val triangle2 = Triangle.getRandomTriangle()
-    triangle2.getData()
+    var entryFlag = true
+    while (entryFlag){
+        val userChoice = gui()
+        when(userChoice){
+            1 -> basicMathOperations()
+            2 -> postfixCalculator()
+            3 -> calculateShapes()
+            4 -> quadraticEquation()
+            5 -> complexNumbers()
+            6 -> {
+                println("Thank you for using our app, see ya next time")
+                entryFlag = false
+            }
+            else -> println("Wrong input, please enter a valid number")
+        }
+    }
+}
+
+fun gui(): Int {
+    for(i in 1..50){
+        print("*")
+    }
+    println()
+    println("   Welcome to pocketMath app")
+    println("1. Basic math operations") //Not implemented
+    println("2. Postfix calculator") //Not implemented
+    println("3. Calculate Shapes")
+    println("4. Quadratic Equation") //Not implemented
+    println("5. Complex numbers") //Not implemented
+    println("6. Exit")
+    println("Your choice: ")
+    val choice = try{ readLine()?.toInt() } catch (e: NumberFormatException) { -1 }
+    for(i in 1..50){
+        print("*")
+    }
+    println()
+    if(choice!= null) return choice
+    else return -1
+}
+
+fun basicMathOperations() {
+    println("Welcome to Basic math operation calculator")
+}
+
+fun postfixCalculator() {
+    println("Welcome to postfix calculator")
+
+}
+
+fun calculateShapes() {
+    println("Welcome to shape calculator")
+}
+
+fun quadraticEquation() {
+    println("Welcome to quadratic equation calculator")
+}
+
+fun complexNumbers() {
+    println("Welcome to complex numbers calculator")
+
 }
