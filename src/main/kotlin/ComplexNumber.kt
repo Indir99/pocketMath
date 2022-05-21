7e27de3831f4741f4e10f0e182970f6a72f90fb9
indir <indir.karabegovic@hotmail.com>
2022-05-21 17:13:10 +0200

    complex number

---
+++ src/main/kotlin/ComplexNumber.kt
@@ -0,0 +1,9 @@
+class ComplexNumber (
+    var realNumber : Double,
+    var imaginaryNumber: Double
+        ) {
+
+        init {
+            println("We created complex number as: $realNumber + i$imaginaryNumber");
+        }
+}
